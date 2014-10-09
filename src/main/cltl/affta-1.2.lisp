70709744f1188f67564bf6463368a0a3abc4d04e
Sean Champ <spchamp@users.sourceforge.net>
2014-10-09 11:12:37 -0700

    add additional, basic monadic and variadic test forms

--- src/main/cltl/affta-1.2.lisp
+++ src/main/cltl/affta-1.2.lisp
@@ -1,11 +1,10 @@
-
 (in-package #:test)
 
 
 (defvar *this*)
 
 (defun do-diadic-test (a b expect pred fn)
-  (let ((results (multiple-value-list 
+  (let ((results (multiple-value-list
                   (funcall fn a b))))
     (signal (cond
               ((funcall pred results expect)
@@ -14,6 +13,28 @@
             :test *this*
             :parameters (list a b)
             :results results)))
-               
-               
-      
+
+
+
+(defun do-monadic-test (a expect pred fn)
+  (let ((results (multiple-value-list
+                  (funcall fn a))))
+    (signal (cond
+              ((funcall pred results expect)
+               (find-class 'test-succeeded))
+              (t (find-class 'test-failed)))
+            :test *this*
+            :parameters (list a)
+            :results results)))
+
+
+(defun do-variadic-test (values expect pred fn)
+  (let ((results (multiple-value-list
+                  (apply fn values))))
+    (signal (cond
+              ((funcall pred results expect)
+               (find-class 'test-succeeded))
+              (t (find-class 'test-failed)))
+            :test *this*
+            :parameters (list a)
+            :results results)))
