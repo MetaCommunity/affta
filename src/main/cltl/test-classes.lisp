3b3b5f7453852fa42b266fcd3f96fb3734f28989
Sean Champ <spchamp@users.sourceforge.net>
2014-10-12 23:00:32 -0700

    FORMAT-TEST-LABEL: PRINC function-name when available

--- src/main/cltl/test-classes.lisp
+++ src/main/cltl/test-classes.lisp
@@ -179,6 +179,17 @@ See also: `DO-TEST-CLEANUP'; `DO-TEST'")
   ;; in which the TEST-OBJECT is assumed to designate a function
   ())
 
+(defmethod format-test-label ((test test) (stream stream))
+  (multiple-value-bind (fn boundp)
+      (slot-value* test 'object)
+    (format stream "[~A] " (class-name (class-of test)))
+    (cond 
+      (boundp
+       (multiple-value-bind (l closure-p name)
+           (function-lambda-expression fn)
+         (princ name stream))))))
+
+
 
 (defclass functional-setf-test (functional-test)
   ;; in which the TEST-OBJECT is assumed to designate a setf form
