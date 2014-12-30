1a68c84905a830504b942131664b67037f1391d1
Sean Champ <gimbal+github@metacommunity.info>
2014-12-30 11:52:09 -0800

    define *TEST-SUITE*, IN-TEST-SUITE, and stubs for additional interface macros

--- src/main/cltl/test-interface.lisp
+++ src/main/cltl/test-interface.lisp
@@ -16,7 +16,7 @@
   (defsuite geometry-test-suite-1
       (:class test-suite))
   
-  (deftest* radians-to-degrees-1 geometry-test-suite-1
+  (deftest radians-to-degrees-1 geometry-test-suite-1
     (:object #'radians-to-degrees)
     (:summary "Ensure...")
     (:setup-lamba ()) ;; no-op
@@ -24,7 +24,9 @@
     (:lambda (theta)
       (radians-to-degrees theta)))
 
-  (defgoals radians-to-degrees-1
+  (in-test-suite geometry-test-suite-1) ;; X
+
+  (defgoals radians-to-degrees-1 (...)
       (:documentation "Ensure...")
     ("Pi Radians => 180 Degrees"
      (:params-form pi)
@@ -55,13 +57,13 @@
     (:depends-on #:info.metacommunity.cltl.utils) ;;;; ?
    )
   
-  (in-test-suite utils-test-suite-1)
+  (in-test-suite utils-test-suite-1) ;; X
 
   (deftest compute-class-1 (ident)
     "Ensure..."
     (compute-class ident))
   
-  (defgoals compute-class-1 
+  (defgoals simple-goals-1 (compute-class-1  )
       (:documentation "Ensure...")
     ("Symbol => Class" 
      (:params-form 'string)
@@ -112,7 +114,6 @@
                    (t (values (car value))))))
              (find-spec (name &optional default)
                (let ((spec (assoc name p :test #'eq)))
-                 (declare (ignore name))
                  (cond 
                    (spec (setf p (delete spec p :test #'eq))
                          (values (format-spec spec) t))
@@ -135,9 +136,60 @@
                                           :name (quote ,name)
                                           :default-test-class ,dtc
                                           ,@(format-properties))))
-           #+TO-DO (register-test-suite ,instance)
+           (register-test-suite ,instance)
            (values ,instance))))))
 
 
 ;; (defsuite foo-suite)
 ;; (defsuite foo-suite (:default-test-class lisp-test))
+
+;; (defclass foo-test-suite (test-suite) ())
+;; (defsuite foo-suite (:class foo-test-suite))
+
+
+(declaim (type test-suite *test-suite*))
+(defvar *test-suite*)
+(setf (documentation *test-suite* 'variable)
+      "Default test suite, within the active lexical environment.
+
+See also: 
+* `TEST-SUITE' [Class]
+* `DEFSUITE' [Macro]
+* `IN-TEST-SUITE' [Macro]
+* `FIND-TEST-SUITE' [Function]")
+
+
+(defmacro in-test-suite (name)
+  "Specify that the test suite named NAME will be bound to
+`*TEST-SUITE*' within the active lexical environment.
+
+NAME will not be evaluated
+
+See also: 
+* `TEST-SUITE' [Class]
+* `DEFSUITE' [Macro]
+* `IN-TEST-SUITE' [Macro]
+* `FIND-TEST-SUITE' [Function]"
+  ;; FIXME: Try to ensure IN-TEST-SUITE will be applied local to:
+  ;; 1. evaluation of a file
+  ;; 2. evaluation of forms from an input stream
+  ;;
+  ;; Consider defining *TEST-SUITE* as a  thread-local variable (TO DO)
+  ;; for this purpose
+  `(setq *test-suite* (find-test-suite (quote ,name))))
+
+
+#+TO-DO
+(defmacro deftest (name (&optional (suite *test-suite*))
+                   &rest properties)
+  ;; mirror DEFSUITE FORMAT-SPEC, FIND-SPEC, FORMAT-PROPERTIES here
+  
+  ;; ...
+  )
+
+#+TO-DO
+(defmacro defgoals (name (test &optional (suite *test-suite*))
+                    &rest properties)
+  ;; mirror DEFSUITE FORMAT-SPEC, FIND-SPEC, FORMAT-PROPERTIES here  
+
+  )
